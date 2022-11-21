de0216cece2d2bec5f6318a1e5b2e519fb7db7b9
Anonym26 <romanfilkov26@gmail.com>
2022-11-21 21:27:48 +0500

    Upload

--- usastores/blog/templatetags/blog_tags.py
+++ usastores/blog/templatetags/blog_tags.py
@@ -1,13 +1,15 @@
 from django import template
 from blog.models import *
 
-
 register = template.Library()
 
 
 # включающий тег
 @register.inclusion_tag('blog/list_categories.html')
 def show_categories(sort=None, cat_selected=0):
+    """
+    Возвращает список категорий.
+    """
     if not sort:
         cats = Category.objects.all()
     else:
@@ -17,8 +19,8 @@ def show_categories(sort=None, cat_selected=0):
 
 @register.inclusion_tag('blog/list_last_post.html')
 def show_last_posts():
-    """Возвращает 3 последних поста."""
+    """
+    Возвращает 3 последних поста.
+    """
     posts = Blog.objects.filter(is_published=True).order_by('-id')[:3]
     return {'posts': posts}
-
-
